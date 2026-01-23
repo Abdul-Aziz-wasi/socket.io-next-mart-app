84dcc83856e47532ccde3ff21b4b939bbfd7d33c
Abdul Aziz <abdulaziz758015@gmail.com>
2026-01-23 14:18:18 +0600

    live map tracking

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import axios from "axios"
 
 dotenv.config()
 const app =express()
+app.use(express.json())
 const server = http.createServer(app)
 const port =process.env.PORT || 5000
 
@@ -27,14 +28,25 @@ io.on("connection",(socket)=>{
             coordinates:[longitude, latitude]
         }
         await axios.post(`${process.env.NEXT_BASE_URL}/api/socket/update-location`,{userId,location})
+        io.emit("update-deliveryBoy-location",{userId,location})
     })
+
+    
 
     socket.on("disconnect",()=>{
         console.log("user disconnected",socket.id)
     })
 })
 
-
+app.post("/notify",(req,res)=>{
+    const {event,data,socketId}=req.body
+    if(socketId){
+        io.to(socketId).emit(event,data)
+    }else{
+        io.emit(event,data)
+    }
+    return res.status(200).json({success:true})
+})
 server.listen(port,()=>{
     console.log("server running at", port)
 })
